4cf6c5d4bab24b39c0c5de72002b3f126a70ecb9
Janez Stupar <janez@janezstupar.com>
2020-10-15 19:12:46 +0200

    Fix errors in windows implementation.

--- go/systray_windows.go
+++ go/systray_windows.go
@@ -11,30 +11,27 @@ func (p *FlutterSystrayPlugin) updateMenu(actions []SystrayAction) {
 		// Adds a GLFW `window.Show` (https://godoc.org/github.com/go-gl/glfw/v3.2/glfw#Window.Show) operation to the
 		// systray menu. It is used to bring window to front.
 		if action.actionType == ActionType.Focus {
-			mShow := systray.AddMenuItem(action.label, action.tooltip)
-			addIcon(action.iconPath, mShow)
+			mShow := systray.AddMenuItem(action.label, "")
 			go func(reference SystrayAction) {
 				for {
 					<-mShow.ClickedCh
-					p.focusHandler(reference)
+					p.focusHandler(&reference)()
 				}
 			}(action)
 		} else if action.actionType == ActionType.Quit {
 			// Set up a handler to close the window
-			mQuit := systray.AddMenuItem(action.label, action.tooltip)
-			addIcon(action.iconPath, mQuit)
-			go func(reference SystrayActions) {
+			mQuit := systray.AddMenuItem(action.label, "")
+			go func(reference SystrayAction) {
 				<-mQuit.ClickedCh
-				p.closeHandler()
+				p.closeHandler(&reference)()
 			}(action)
 		} else if action.actionType == ActionType.SystrayEvent {
-			mEvent := systray.AddMenuItem(action.label, action.tooltip)
-			addIcon(action.iconPath, mEvent)
+			mEvent := systray.AddMenuItem(action.label, "")
 			// Set up a callback handler
 			go func(reference SystrayAction) {
 				for {
 					<-mEvent.ClickedCh
-					p.eventHandler(reference)
+					p.eventHandler(&reference)()
 				}
 			}(action)
 		}
@@ -48,11 +45,11 @@ func initialize(title string, iconData []byte) {
 		}
 
 		if len(title) > 0 {
-			systray.SetTitle(mainEntry.title)
+			systray.SetTitle(title)
 		}
 	}
 
 	go func() {
-		systray.Run(readyFunc, systrayOnExit)
+		systray.Run(readyFunc, nil)
 	}()
 }
